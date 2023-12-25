28cfccf38820c837b484c6fcef9817c179e1a62e
Pratham Pawar <prathampawar9775@gmail.com>
2023-12-25 23:08:45 +0530

    update

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-
 mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
-
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const postSchema = mongoose.Schema({
   picture: String,
   user: {
